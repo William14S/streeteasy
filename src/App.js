587bd7d9dbda1e598e069a1b1e30d13b0e33569a
William14S <wsandoval7645@richmondhillhs.org>
2021-02-11 21:07:02 +0000

    final

--- src/App.js
+++ src/App.js
@@ -10,29 +10,40 @@ function App() {
       <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
 
       <DataView
-       borough="Staten Island"
-       renterCost="$14,292"
-       ownerCost="$29,752"/>
+      borough="Staten Island"
+      renterCost="$14,292"
+      ownerCost="$29,752"
+      renterIncome="$37,882"
+      ownerIncome="$94,177"/>
 
              <DataView
        borough="Queens"
-       renterCost="$14,292"
-       ownerCost="$29,752"/>
+       renterCost="$16,812"
+       ownerCost="$29,256"
+       renterIncome="$46,549"
+      ownerIncome="$80,144"/>
 
              <DataView
        borough="Manhattan"
-       renterCost="$14,292"
-       ownerCost="$29,752"/>
+       renterCost="$18,900"
+       ownerCost="$36,252"
+       renterIncome="$62,173"
+      ownerIncome="$142,046"/>
 
              <DataView
        borough="Brooklyn"
-       renterCost="$14,292"
-       ownerCost="$29,752"/>
+       renterCost="$15,144"
+       ownerCost="$31,908"
+       renterIncome="$40,128"
+      ownerIncome="$83,737"/>
 
              <DataView
        borough="Bronx"
-       renterCost="$14,292"
-       ownerCost="$29,752"/>
+       renterCost="$13,176"
+       ownerCost="$28,548"
+       renterIncome="$29,302"
+      ownerIncome="$76,454"/>
+
     </div>
   );
 }
